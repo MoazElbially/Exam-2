ca1f68f029c0b6b2eb5a73b5f552e3d8479a766d
MoazElbially <mezoelbially44@gmail.com>
2026-09-18 22:13:54 +0300

    Add MCQQuestion & True Or FalseQuestion Classes

---
+++ MCQQuestion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_2
+{
+    internal class MCQQuestion : Question
+    {
+        public MCQQuestion(string header, string body, double mark, Answer[] answers, Answer rightAnswer) : base(header, body, mark)
+        {
+            AnswerList = answers;
+            RightAnswer = rightAnswer;
+        }
+        public override void DisplayQuestion()
+        {
+            Console.WriteLine(Header);
+            Console.WriteLine(Body);
+
+            foreach (Answer answer in AnswerList)
+            {
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
---
+++ TrueFalseQuestion.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_2
+{
+    public class TrueFalseQuestion : Question
+    {
+
+        public TrueFalseQuestion(string header, string body, double mark, Answer[] answers, Answer rightAnswer) : base(header, body, mark)
+
+        {
+
+            {
+                AnswerList = answers;
+                RightAnswer = rightAnswer;
+            }
+            ;
+
+        }
+        public override void DisplayQuestion()
+        {
+            Console.WriteLine(Header);
+            Console.WriteLine(Body);
+            foreach (Answer answer in AnswerList)
+            {
+                Console.WriteLine(answer);
+            }
+        }
+    }
+}
