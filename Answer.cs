bd321f17f4d34f87244cf7384e795a040aebc2a8
MoazElbially <mezoelbially44@gmail.com>
2026-09-18 22:00:13 +0300

    Add Answer Class

---
+++ Answer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_2
+{
+    public class Answer
+    {
+        public int AnswerId { get; set; }
+        public string AnswerText { get; set; }
+        public Answer()
+        {
+            AnswerId = 0;
+            AnswerText = "";
+
+        }
+        public Answer(int answerId, string answerText)
+        {
+            AnswerId = answerId;
+            AnswerText = answerText;
+        }
+        public override string ToString()
+        {
+            return $"{AnswerId} {AnswerText}";
+        }
+    }
+}
