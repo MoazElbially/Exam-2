ea7893df251b9993b2e8fa41249a6302b148daaa
MoazElbially <mezoelbially44@gmail.com>
2026-09-18 22:08:17 +0300

    Add Exam & Question Classes

---
+++ Exam.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_2
+{
+    public abstract class Exam
+    {
+        public int Time { get; set; }
+        public int NumberOfQuestions { get; set; }
+
+        public Question[] Questions { get; set; }
+        protected Exam(int time, int numberOfQuestions)
+        {
+            Time = time;
+            NumberOfQuestions = numberOfQuestions;
+            Questions = new Question[numberOfQuestions];
+        }
+        public abstract void ShowExam();
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Question.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_2
+{
+    public abstract class Question : ICloneable, IComparable<Question>
+    {
+        public string Header { get; set; }
+        public string Body { get; set; }
+        public double Mark { get; set; }
+
+        public Answer[] AnswerList { get; set; }
+
+        public Answer RightAnswer { get; set; }
+
+
+        protected Question() : this("", "", 0)
+        {
+        }
+
+        protected Question(string header, string body, double mark)
+        {
+            Header = header;
+            Body = body;
+            Mark = mark;
+            AnswerList = new Answer[0];
+            RightAnswer = new Answer();
+        }
+
+        public abstract void DisplayQuestion();
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+        public int CompareTo(Question? other)
+        {
+            if (other == null)
+                return 1;
+
+            return Mark.CompareTo(other.Mark);
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Header}: {Body} : Mark = {Mark}";
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
