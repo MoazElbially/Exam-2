fa9ee268f5ce40e17f19e76833ba272533db5e88
MoazElbially <mezoelbially44@gmail.com>
2026-09-18 22:19:41 +0300

    Add PracticalExam & Subject Classes

---
+++ PracticalExam.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_2
+{
+    public class PracticalExam : Exam
+    {
+
+        public PracticalExam(int time, int numberOfQuestions) : base(time, numberOfQuestions)
+
+        {
+        }
+
+        public override void ShowExam()
+        {
+            Console.WriteLine();
+            Console.WriteLine("=================================");
+            Console.WriteLine("        PRACTICAL EXAM");
+            Console.WriteLine("=================================");
+
+
+
+            for (int i = 0; i < Questions.Length; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Question {i + 1}");
+                Console.WriteLine("---------------------------------");
+
+                Questions[i].DisplayQuestion();
+
+                Console.Write("Enter your answer ID: ");
+                int userAnswerId = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("=================================");
+            Console.WriteLine("       CORRECT ANSWERS");
+            Console.WriteLine("=================================");
+
+            for (int i = 0; i < Questions.Length; i++)
+            {
+                Console.WriteLine($"Question {i + 1}: " + $"{Questions[i].RightAnswer.AnswerText}");
+
+            }
+            Console.WriteLine("=================================");
+
+        }
+    }
+}
---
+++ Subject.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exam_2
+{
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+
+        public Exam Exam { get; private set; }
+
+
+        public Subject(int subjectId, string subjectName)
+        {
+            SubjectId = subjectId;
+            SubjectName = subjectName;
+        }
+
+
+        public void CreateExam(Exam exam)
+
+
+        {
+            Exam = exam;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{SubjectId} : {SubjectName}";
+        }
+    }
+}
